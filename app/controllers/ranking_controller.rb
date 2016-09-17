d9b9463e985bf9b750ebdbe1fec061eabd897f53
ana <syuzyuzyu@gmail.com>
2016-09-17 09:59:40 +0000

    submission report

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -12,10 +12,6 @@ class RankingController < ApplicationController
   def ranking(type)
     #postgreでNG
     #@items = Item.joins(:ownerships).where('ownerships.type = ?', type).group(:item_id).distinct.order('count(item_id) desc').limit(10)
-
     @items = Item.find_by_sql(['SELECT DISTINCT "items".*, count FROM "items" LEFT JOIN ( select "item_id", count(item_id) as count from "ownerships"  WHERE (ownerships.type = ?) GROUP BY "item_id" ) "rank" ON "rank"."item_id" = "items"."id" ORDER BY "count" desc limit 10', type])
-    
-    #ows = Ownership.where('ownerships.type = "Wnat"')
-    #@items = Item.where(ows).group(:item_id).order('count(id) desc').limit(10)
   end
 end
