d8bdb39944de3d1031b1345db6a73e3a419628a1
ana <syuzyuzyu@gmail.com>
2016-09-17 06:54:02 +0000

    return

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -11,8 +11,7 @@ class RankingController < ApplicationController
 
   def ranking(type)
     #posgreでNG
-    #@items = Item.joins(:ownerships).where('ownerships.type = ?', type).group(:item_id).distinct.order('count(item_id) desc').limit(10)
-    @items = Item.joins(:ownerships).where('ownerships.type = ?', type).group('items.id').distinct.order('count(item_id) desc').limit(10)
+    @items = Item.joins(:ownerships).where('ownerships.type = ?', type).select('items.*').group(:item_id).distinct.order('count(item_id) desc').limit(10)
 
     #ows = Ownership.where('ownerships.type = "Wnat"')
     #@items = Item.where(ows).group(:item_id).order('count(id) desc').limit(10)
