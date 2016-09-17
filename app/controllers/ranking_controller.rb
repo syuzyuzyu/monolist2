f0ff4e2ff9cb427cacbffed5829105ea83eb265c
ana <syuzyuzyu@gmail.com>
2016-09-17 09:17:56 +0000

    compromise

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -10,9 +10,11 @@ class RankingController < ApplicationController
   private
 
   def ranking(type)
-    #posgreでNG
-    @items = Item.joins(:ownerships).where('ownerships.type = ?', type).select('items.*').group(:item_id).distinct.order('count(item_id) desc').limit(10)
+    #postgreでNG
+    #@items = Item.joins(:ownerships).where('ownerships.type = ?', type).group(:item_id).distinct.order('count(item_id) desc').limit(10)
 
+    @items = Item.find_by_sql(['SELECT DISTINCT "items".* FROM "items" INNER JOIN "ownerships" ON "ownerships"."item_id" = "items"."id" WHERE (ownerships.type = ?) GROUP BY "item_id"  ORDER BY count(item_id) desc limit 10', type])
+    
     #ows = Ownership.where('ownerships.type = "Wnat"')
     #@items = Item.where(ows).group(:item_id).order('count(id) desc').limit(10)
   end
